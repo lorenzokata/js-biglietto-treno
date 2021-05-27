79f872fd13c5fbb7f9340ad92a846b02aa70e7d0
lorenzokata <catalanolorenzo95@gmail.com>
2021-05-27 16:16:37 +0200

    es completato

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@
 // Questo richiederà un minimo di ricerca.
 
 
+var numKm = prompt('Quanti km vuoi percorrere?');
+var eta = prompt('Quanti hanni hai');
+var prezzo = numKm * 0.21
+
+if (eta < 18) {
+    prezzo *= 0.8;
+}
+else if (eta > 65) {
+    prezzo *= 0.6;
+}
+
+alert('Il prezzo del biglietto è '+ (Math.round(prezzo * 100) / 100).toFixed(2) + '€');
